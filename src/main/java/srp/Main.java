a0e9d1789820e4d131a72b8a00f3ebf2f27ce2c7
Vasconcelos Silvio <silviovasconcelos91@gmail.com>
2025-06-30 18:11:02 +0200

    Main update

--- src/main/java/srp/Main.java
+++ src/main/java/srp/Main.java
@@ -4,7 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         Invoice invoice = new Invoice("Alice Dupont", 100.0);
-        invoice.saveToDatabase();
-        invoice.print();
+
+        InvoiceRepository repository = new InvoiceRepository();
+        repository.save(invoice);
+
+        InvoicePrinter printer = new InvoicePrinter();
+        printer.printPDF(invoice);
     }
 }
